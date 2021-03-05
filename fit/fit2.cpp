30c5894c92f708242dbd5739192d57c0aefb7ad0
giorgio <giorgio.pizzati@gmail.com>
2021-03-05 09:42:14 +0100

    update

--- fit/fit2.cpp
+++ fit/fit2.cpp
@@ -28,8 +28,13 @@ int main(int argc, char** argv)
         CfgParser * gConfigParser = new CfgParser (argv[1]);
         string name = gConfigParser->readStringOpt("general::name");
 
+        int function_type = gConfigParser->readIntOpt (name+"::f_type");
+
+
         string filename = gConfigParser->readStringOpt(name+"::filename");
 
+        string title = gConfigParser->readStringOpt(name+"::title");
+        
         vector<float> parameters = gConfigParser->readFloatListOpt (name+"::parameters");
 
         vector<float> range = gConfigParser->readFloatListOpt (name+"::range");
@@ -43,8 +48,15 @@ int main(int argc, char** argv)
         TCanvas* cnv = new TCanvas("myC1","myC1",10,10,1200,800);
         gStyle->SetOptFit(1111);
         //TF1 * func = new TF1("fun", "[0]*TMath::Exp(-x[0]/[1])",0.5, 10);
-        TF1 * func = new TF1("fun", "[0]*TMath::Exp(-x[0]/[1]) +  [2]*TMath::Exp(-x[0]/[3])+ [4]",range[0], range[1]);
-        // double neg_muon = 5164*0.44;
+        TF1 * func;
+        if(function_type==1){
+            func = new TF1("fun", "[0]*TMath::Exp(-x[0]/[1]) + [2]",range[0], range[1]);
+
+        }
+        else if(function_type==2){
+            func = new TF1("fun", "[0]*TMath::Exp(-x[0]/[1]) +  [2]*TMath::Exp(-x[0]/[3])+ [4]",range[0], range[1]);
+        }
+                // double neg_muon = 5164*0.44;
         // double pos_muon = 5164*0.56;
         // double neg_tau = 2.005;
         // double pos_tau = 2.2;
@@ -83,12 +95,14 @@ int main(int argc, char** argv)
         double t = abs(tau-func->GetParameter(1))/r->ParError(1);
         cout << "t: " << t <<endl;
 
-        stringstream s;
+        /*stringstream s;
         s << "Vita media: " << round(func->GetParameter(1)*1000)/1000 << " +/- " <<  round(r->ParError(1)*1000)/1000 << " microsec. t: " << round(t*1000)/1000;
         h->SetTitle(s.str().c_str());
+        */
+        h->SetTitle(title.c_str());
         cnv->Modified();
         cnv->Update();
-        cnv->Print("fit_vita_media.png", "png");
+        //cnv->Print("fit_vita_media.png", "png");
         myApp->Run();
         return 0;
 }
