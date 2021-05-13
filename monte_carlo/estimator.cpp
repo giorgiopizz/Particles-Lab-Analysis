2b8f9c28508fae3b248966205f906dc34c0c0633
Giorgio Pizzati <giorgio.pizzati@gmail.com>
2021-05-13 21:46:13 +0200

    monte carlo now works!

--- monte_carlo/estimator.cpp
+++ monte_carlo/estimator.cpp
@@ -27,7 +27,7 @@ std:0.0109906
 
 using namespace std;
 
-double time_offset = 0.000;
+double time_offset = 0.00;
 
 
 void histo_populate(TH1F * h, int n_tot, double tau){
@@ -69,19 +69,20 @@ int main(int argc, char** argv)
 
     std::vector<double> tau;
     std::vector<double> chi_square;
-    int experiments = 100000;
-    int counts = 5000;
-
+    int experiments = 10000;
+    int counts = 100000;
+    gStyle->SetOptStat(11);
+    gStyle->SetOptFit(1111);
     TRandom3 * r = new TRandom3();
     int n = r->Poisson(counts);
 
     TF1 * func = new TF1("fun", "[0]*TMath::Exp(-x[0]/[1])",0.0+time_offset, 11);
-    func->SetParameter(0,counts/2);
-    func->SetParameter(1,2.1);
+    func->SetParameter(0,n/2);
+    func->SetParameter(1,2.100);
     // func->SetParameter(2,2.2);
     // func->SetParLimits(2,0.0,100);
 
-    // TApplication* myApp = new TApplication("myApp", NULL, NULL);
+    TApplication* myApp = new TApplication("myApp", NULL, NULL);
 
     // double f_mu_pos = r->Gaus(0.56075, 0.00087);
     double f_mu_pos = 0.56075;
@@ -89,12 +90,12 @@ int main(int argc, char** argv)
     double f_mu_neg = 1 - f_mu_pos;
 
     for(int i=0;i<experiments;i++){
-        TCanvas* cnv = new TCanvas(("myC1" + to_string(i)).c_str(),"myC1",10,10,1200,800);
+        // TCanvas* cnv1 = new TCanvas(("myC1" + to_string(i)).c_str(),"myC1",10,10,1200,800);
         // generate each pseudoexperiment
         // in a single experiment we populate an histogram with the histo_populate
         // method, we then fit this histogram in order to estimate tau,
         double min=0, max=11;
-        int bin = 20;
+        int bin = 100;
         TH1F *h = new TH1F("h", "example histogram",bin,min,max);
 
 
@@ -102,8 +103,8 @@ int main(int argc, char** argv)
 
 
         int mu_pos = (int) r->Binomial(n, f_mu_pos);
-        // int mu_neg = (int) r->Binomial(n, f_mu_neg);
-        int mu_neg = n - mu_pos;
+        int mu_neg = (int) r->Binomial(n, f_mu_neg);
+        // int mu_neg = n - mu_pos;
 
         // cout << mu_pos << " " << mu_neg << endl;
 
@@ -115,8 +116,9 @@ int main(int argc, char** argv)
         // histo_populate_background(h, 0.2*n);
 
 
-        // cnv->cd();
+        // cnv1->cd();
         // h->Draw();
+
         TFitResultPtr result = h->Fit(func, "SQR");
         if(result->Status()!=0){
             cout << "Error!" << endl;
@@ -128,7 +130,8 @@ int main(int argc, char** argv)
         chi_square.push_back(result->Chi2());
 
         delete h;
-
+        // cnv1->Modified();
+        // cnv1->Update();
     }
 
     double mu = mean(tau);
@@ -136,12 +139,12 @@ int main(int argc, char** argv)
 
     cout << "Tau statistics:\nmu:" << mu << "\nstd:" << std << endl;
 
-    TApplication* myApp = new TApplication("myApp", NULL, NULL);
+
     TCanvas* cnv = new TCanvas("myC1","myC1",10,10,1200,800);
     gStyle->SetOptFit(1111);
     double min = *min_element(tau.begin(), tau.end());
     double max = *max_element(tau.begin(), tau.end());
-    TH1F *h = new TH1F("h", "tau_MC",40, min, max);
+    TH1F *h = new TH1F("h", "tau_MC",50, min, max);
 
     for(double t: tau)   h->Fill(t);
 
@@ -152,18 +155,18 @@ int main(int argc, char** argv)
     cnv->Update();
     cnv->SaveAs("images/tau_MC.png");
 
-    // TCanvas* cnv1 = new TCanvas("myC2","myC2",10,10,1200,800);
-    // gStyle->SetOptFit(1111);
-    // double min1 = *min_element(chi_square.begin(), chi_square.end());
-    // double max1 = *max_element(chi_square.begin(), chi_square.end());
-    // TH1F *h1 = new TH1F("h1", "chi_square",60, min1, max1);
-    //
-    // for(double t: chi_square)   h1->Fill(t);
-    //
-    // h1-> Draw();
-    // h1->Fit(func2);
-    // cnv1->Modified();
-    // cnv1->Update();
-    // cnv1->SaveAs("images/chi_MC.png");
+    TCanvas* cnv1 = new TCanvas("myC2","myC2",10,10,1200,800);
+    gStyle->SetOptFit(1111);
+    double min1 = *min_element(chi_square.begin(), chi_square.end());
+    double max1 = *max_element(chi_square.begin(), chi_square.end());
+    TH1F *h1 = new TH1F("h1", "chi_square",60, min1, max1);
+
+    for(double t: chi_square)   h1->Fill(t);
+
+    h1-> Draw();
+    h1->Fit(func2);
+    cnv1->Modified();
+    cnv1->Update();
+    cnv1->SaveAs("images/chi_MC.png");
     myApp->Run();
 }
